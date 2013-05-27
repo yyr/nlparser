32d27cbc1083bfd1a26a1819268ead5bb1d0b35f
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2013-05-27 11:01:27 +0900

    * nlparser/__init__.py: source loading helpers

--- nlparser/__init__.py
+++ nlparser/__init__.py
@@ -8,25 +8,36 @@
     nlparser - Python module to manipulate Fortran namelist files.
 ==========================================================================
 
+"""
 # [[[end]]]
+
 DATE    = "Thursday, May 25 2013"
 AUTHOR  = "Yagnesh Raghava Yakkala"
 WEBSITE = "http://github.com/yyr/nlparser"
 LICENSE = "GPL v3 or later"
 VERSION = '0.1-dev'
 
-from .core import NameList
+import codecs
+from .core import Namelist
 
 
-def parse_lines(lines, filename):
-    """
-    """
+def _parse_lines(lines, filename):
     return Namelist(lines, filename=filename)
 
 
+def load(filename):
+    nlfile = codecs.open(filename, encoding='utf8')
+    return loadi([ l.rstrip('\n') for l in nlfile.readlines()],
+                 "<file>")
 
 
+def loads(string):
+    return loadi(string.splitlines(), "<lines>")
 
+
+def loadi(lines,filename):
+    print(lines)
+    return _parse_lines(lines, filename)
 
 if __name__ == '__main__':
-    main()
+    pass
